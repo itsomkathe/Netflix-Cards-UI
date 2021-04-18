dc322f24cb029b413e395952e89a1681c18b7b32
Om Kathe <omkathe26@gmail.com>
2021-04-18 13:04:43 +0530

    Updates

---
+++ precache-manifest.8544017e334078fdd7067ec9dfd20558.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5adfc904a836f0c907b2248e857cc6c6",
+    "url": "/Netflix-Cards-UI/index.html"
+  },
+  {
+    "revision": "3d343d5ecee01c7fe5d6",
+    "url": "/Netflix-Cards-UI/static/css/main.c0a8b9c5.chunk.css"
+  },
+  {
+    "revision": "77f0c01f2a8a2fc49165",
+    "url": "/Netflix-Cards-UI/static/js/2.6b391706.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Netflix-Cards-UI/static/js/2.6b391706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d343d5ecee01c7fe5d6",
+    "url": "/Netflix-Cards-UI/static/js/main.236789a8.chunk.js"
+  },
+  {
+    "revision": "401e7c99f9599483fbbe",
+    "url": "/Netflix-Cards-UI/static/js/runtime-main.a30d9fa9.js"
+  }
+]);
